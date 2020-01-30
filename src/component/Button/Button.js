be4d95386b9af7b89a4911fd5a379ad530b85148
Евгений Самохвалов Николаевич <@LATSPB.ru>
2020-01-30 16:55:04 +0300

    fix: state and setState

--- src/component/Button/Button.js
+++ src/component/Button/Button.js
@@ -9,48 +9,37 @@ class Button extends Component {
         super();
 
         this.state = {
-            stateButton: 'Off'
+            stateButton: false
         }
 
         axios.get('https://off-on-3e272.firebaseio.com/button.json')
         .then(
             response => {
-                console.log(response.data.stateButton)
                 this.setState({stateButton:response.data.stateButton})
             }
         )
     }
 
     switchButton = () => {
-        this.state.stateButton === 'Off' ?
-        this.setState({ stateButton: 'On'}):
-        this.setState({ stateButton: 'Off'})
+        this.setState({ stateButton: !this.state.stateButton})
     }
 
     render() {
 
         return (
-            <button className={this.state.stateButton === 'Off'? 'Button Off': 'Button On'} onClick={this.switchButton}>
-                {this.state.stateButton}
+            <button className={this.state.stateButton ? 'Button On': 'Button Off'} onClick={this.switchButton}>
+                { this.state.stateButton ?'On':'Off' }
             </button>
         ) 
     }
 
     componentDidUpdate() {
-
         axios.put('https://off-on-3e272.firebaseio.com/button.json', this.state)
-        .then(response => {
-
-        })
-
     }
 
     componentWillUnmount() {
-        this.setState({ stateButton: 'Off'})
+        this.setState({ stateButton: false})
         axios.put('https://off-on-3e272.firebaseio.com/button.json', this.state)
-        .then(response => {
-
-        })
     }
 }
 
@@ -74,3 +63,7 @@ export default Button
 // off-on-3e272
 
 // https://off-on-3e272.firebaseio.com/
+
+
+
+// ываываы
