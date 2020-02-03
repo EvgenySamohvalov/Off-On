c25ccf6613ebd762e1aecda8a89dcce929d273ae
Евгений Самохвалов Николаевич <@LATSPB.ru>
2020-02-03 14:42:22 +0300

    feat: realize store on redux

--- src/component/Button/Button.js
+++ src/component/Button/Button.js
@@ -2,68 +2,43 @@ import React, {Component} from 'react'
 
 import './Button.css'
 import axios from 'axios'
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 
 class Button extends Component {
     constructor() {
         super();
-
-        this.state = {
-            stateButton: false
-        }
-
         axios.get('https://off-on-3e272.firebaseio.com/button.json')
         .then(
             response => {
-                this.setState({stateButton:response.data.stateButton})
+                this.props.setFirstStateButton(response.data.stateButton)
             }
         )
     }
 
-    switchButton = () => {
-        this.setState({ stateButton: !this.state.stateButton})
-    }
-
     render() {
-
         return (
-            <button className={this.state.stateButton ? 'Button On': 'Button Off'} onClick={this.switchButton}>
-                { this.state.stateButton ?'On':'Off' }
+            <button className={this.props.stateButton ? 'Button On': 'Button Off'} onClick={this.props.switchButton}>
+                { this.props.stateButton ?'On':'Off' }
             </button>
         ) 
     }
 
     componentDidUpdate() {
-        axios.put('https://off-on-3e272.firebaseio.com/button.json', this.state)
-    }
-
-    componentWillUnmount() {
-        this.setState({ stateButton: false})
-        axios.put('https://off-on-3e272.firebaseio.com/button.json', this.state)
+        axios.put('https://off-on-3e272.firebaseio.com/button.json', this.props.stateButton)
     }
 }
 
-// function mapStateToProps(state) {
-//     return {
-//         stateButton: state.button.stateButton
-//     }
-// }
+function mapStateToProps(state) {
+    return {
+        stateButton: state.button.stateButton
+    }
+}
 
-// function mapDispatchToProps(dispatch) {
-//     return {
-//         fetchButton: () => dispatch(fetchButton())
-//     }
-// }
+function mapDispatchToProps(dispatch) {
+    return {
+        switchButton: () => dispatch({type:'SWITCH'}),
+        setFirstStateButton: state => dispatch({type:'SFSB', payload:state})
+    }
+}
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Button)
-
-export default Button
-
-
-// off-on-3e272
-
-// https://off-on-3e272.firebaseio.com/
-
-
-
-// ываываы
+export default connect(mapStateToProps, mapDispatchToProps)(Button)
--- src/store/reducers/button.js
+++ src/store/reducers/button.js
@@ -1,9 +1,18 @@
 const initialState = {
-    stateButton: 'Off'
+    stateButton: false
 }
 
 export default function buttonReducer(state = initialState, action) {
     switch (action.type) {
+        case 'SWITCH':
+            return {
+                stateButton: !state.stateButton
+            }
+        case 'SFSB': 
+            return {
+                stateButton: action.payload
+            }
+        
         default: 
             return state
     }
